cac48aca7e8d2512a1bad620481cb4a195004db8
Nina <nm.nysya@gmail.com>
2024-01-28 00:43:34 +0200

    add cycle (while True) under function

--- task_2.py
+++ task_2.py
@@ -1,10 +1,25 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    list_numbers = random.sample(range(min, max), quantity)
-    list_numbers = sorted(list_numbers)
-    return list_numbers
+        list_numbers = random.sample(range(min, max), quantity)
+        list_numbers = sorted(list_numbers)
+        return list_numbers
 
 
-lottery_numbers = get_numbers_ticket(min=1, max=50, quantity=5)
-print("Ваші лотерейні числа:", lottery_numbers)
+
+while True:
+
+    min = int(input("Мінімальний номер білету >= 1: "))
+    max = int(input("Максимальний номер білету <= 1000: "))
+
+    if min >= 1 and max <= 1000:
+        quantity = int(input("Кількість унікальних чисел для лотерейного квитка: "))
+        lottery_numbers = get_numbers_ticket(min, max, quantity)
+        print("Ваші лотерейні білети:", lottery_numbers)
+        break
+    else:
+        not_valid_range = input("На жаль, введені числа не відповідають заданому діапазону.Спробуєте ще раз?"
+                                "\n1.Так\n2.Ні\n>>>Відповідь користувача: ")
+
+        if not_valid_range == "2":
+            break
